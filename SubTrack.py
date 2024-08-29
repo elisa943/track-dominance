2cbaa0e2f4da1ac65a7cb6b26afdf24e4d93569e
elisa <68431568+elisa943@users.noreply.github.com>
2024-08-29 14:33:52 +0200

    Create SubTrack.py

---
+++ SubTrack.py
@@ -0,0 +1,8 @@
+import fastf1 as ff
+from fastf1.plotting import *
+from matplotlib import pyplot as plt
+
+class SubTrack(Track):
+  def __init__(self, index):
+    Track.__init__(self) # super().__init__()
+    self.index = index
