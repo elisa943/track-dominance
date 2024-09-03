2b0ae2797f70cfc984809d877c9d3ac4b2c5626d
Elisa Chien <echien@bordeaux-inp.fr>
2024-09-03 17:01:15 +0200

    Clean code and add color to track display

--- DisplayTrack.py
+++ DisplayTrack.py
@@ -1,21 +1,23 @@
 import fastf1 as ff
 from fastf1.plotting import *
+import numpy as np
 from matplotlib import pyplot as plt
 from Track import *
 
 class DisplayTrack(Track):
-    def __init__(self, session, circuit, driver=None):
+    def __init__(self, session, circuit, driver_color, driver_name=None, ):
         self.session = session
         self.circuit = circuit 
-        self.driver = driver
+        self.driver_name = driver_name
+        self.driver_color = driver_color # RGB value 
         
     def plot(self):
 
         # Plot circuit (with fastest lap) https://docs.fastf1.dev/gen_modules/examples_gallery/plot_annotate_corners.html#sphx-glr-gen-modules-examples-gallery-plot-annotate-corners-py
         fastest_lap = self.session.laps.pick_fastest()
         
-        if self.driver != None: 
-            fastest_lap = self.session.laps.pick_driver(self.driver).pick_fastest()
+        if self.driver_name != None: 
+            fastest_lap = self.session.laps.pick_driver(ff.plotting.get_driver_abbreviation(self.driver_name, self.session)).pick_fastest()
         
         tel = fastest_lap.get_telemetry()
         pos = fastest_lap.get_pos_data()
@@ -29,9 +31,11 @@ class DisplayTrack(Track):
 
         # Rotate and plot the track map.
         rotated_track = DisplayTrack.rotate(self, track, angle=track_angle)
-        plt.plot(rotated_track[:, 0], rotated_track[:, 1])
+        plt.plot(rotated_track[:, 0], rotated_track[:, 1], color=self.driver_color)
 
         # Plot
+        print(self.driver_name)
+        plt.legend([self.driver_name])
         plt.title(self.session.event['Location'])
         plt.xticks([])
         plt.yticks([])
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # from tkinter import * # Listbox widget 
 import numpy as np
+import os
 from fastf1.ergast import Ergast
 from DisplayTrack import *
 from datetime import datetime
@@ -7,17 +8,25 @@ from datetime import datetime
 def clear(year: int):
     clear_cache(cache_dir=str(year))
 
+def clear_terminal():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
 def check_season(season_selected: int):
     current_year = datetime.now().year
     return season_selected >= 1950 and season_selected <= current_year
 
-def check_circuit(circuit_selected: str, circuitsId_season_selected):
-    return circuit_selected.lower() in circuitsId_season_selected
+def check_circuit(circuit_selected: int, circuitsId_season_selected):
+    return 0 <= circuit_selected < len(circuitsId_season_selected)
 
 def check_driver(driver: str, drivers_names, drivers_abr):
     return driver.lower() in drivers_names or driver.upper() in drivers_abr
 
+def display_drivers_list(circuitsId_season_selected):
+    for i in range(len(circuitsId_season_selected)):
+        print(i, circuitsId_season_selected[i])
+
 def main():
+    clear_terminal()
     ergast = Ergast()
     type_of_session = 'Race'
     type_result = 'pandas'
@@ -27,29 +36,37 @@ def main():
     if not check_season(season_selected): 
         print("Pick a year between 1950 and the current year.")
         return
+    
     # Get all circuits from season selected by user  
-    circuitsId_season_selected = ergast.get_circuits(season=season_selected)["circuitId"].to_numpy()
-    print(circuitsId_season_selected)
+    circuitsId_season_selected = ergast.get_circuits(season=season_selected, result_type=type_result)["circuitId"].tolist()
+    
     # Pick circuit
-    circuit_selected = input("Which circuit ? ")
-    if not check_circuit(circuit_selected, circuitsId_season_selected): 
+    clear_terminal()
+    display_drivers_list(circuitsId_season_selected)
+    circuit_selected = int(input("Which circuit (enter number) ? "))
+    if not check_circuit(circuit_selected, circuitsId_season_selected):
         print("Pick a circuit from the season.")
         return 
+    circuit_selected = circuitsId_season_selected[circuit_selected]
 
     session_selected = ff.get_session(season_selected, circuit_selected, type_of_session)
     session_selected.load()
+    
     event = ff.get_event(season_selected, circuit_selected)
     circuit = session_selected.get_circuit_info()
-
-    # Drivers who raced that weekend
-    drivers_info = ergast.get_driver_info(season=season_selected, circuit=circuit_selected, result_type=type_result).values
+    
+    # Drivers who raced that weekend # TODO : BUG FOUND NOT FIXED
+    #drivers_info = ergast.get_driver_info(season=season_selected, circuit=circuit_selected, result_type=type_result)
+    drivers_info = ergast.get_driver_info(season=2023, circuit='baku', result_type=type_result).to_numpy()
+    print(drivers_info)
     num_drivers = len(drivers_info)
     drivers_abr = [drivers_info[i][2] for i in range(num_drivers)]
-    drivers_names = [drivers_info[i][5] + " " + drivers_info[i][4] for i in range(num_drivers)]
+    drivers_names = [drivers_info[i][4] + " " + drivers_info[i][5] for i in range(num_drivers)]
+    abr_to_name = {drivers_abr[i]: drivers_names[i] for i in range(num_drivers)}
     drivers_colors = [ff.plotting.driver_color(drivers_abr[i]) for i in range(num_drivers)] 
-    driver_to_color = {drivers_names[i]: drivers_colors[i] for i in range (num_drivers)}
+    driver_to_color = {drivers_abr[i]: drivers_colors[i] for i in range (num_drivers)}
 
-    print(drivers_names)
+    clear_terminal()
     print(drivers_abr)
     driver_selected_1 = input("Pick a driver : ")
     driver_selected_2 = input("Pick a second driver : ")
@@ -57,8 +74,8 @@ def main():
         print("Pick a driver from the list")
         return 
     
-    displayTrack_driver_1 = DisplayTrack(session_selected, circuit, driver_selected_1)
-    displayTrack_driver_2 = DisplayTrack(session_selected, circuit, driver_selected_2)
+    displayTrack_driver_1 = DisplayTrack(session_selected, circuit, driver_to_color[driver_selected_1], abr_to_name[driver_selected_1])
+    displayTrack_driver_2 = DisplayTrack(session_selected, circuit, driver_to_color[driver_selected_2], abr_to_name[driver_selected_2])
 
     displayTrack_driver_1.plot()
     displayTrack_driver_2.plot()
